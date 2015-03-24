9330f3deaa2ff937e412d621e08e370e9cfbb74e
Daniel Stern <daniel@danielstern.ca>
2015-03-23 21:34:29 -0400

    integrate cyo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,21 @@
-angular.module('EB2',['ui.router'])
+angular.module('EB2',['ui.router','cyo'])
 .config(function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider.state({
+    $stateProvider
+    	.state({
             name: "main",
             url: "/",
             templateUrl: 'scripts/templates/main.html',
             controller:function(){
             	console.log("HI")
+            }
+        })
+    	.state({
+            name: "town",
+            url: "/town",
+            templateUrl: 'scripts/templates/town.html',
+            controller:function(){
+            	
             }
         })
 
